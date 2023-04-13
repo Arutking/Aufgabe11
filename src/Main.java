dde8cb24d4a5156f2b48b8b83a73a4753980f037
Arut Aerian <arut.aerian@icloud.com>
2023-04-13 16:33:16 +0200

    Aufgabe11.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int max = 1000;
+    int min = 1;
+    int temp;
+
+    int guess = (max + min) / 2;
+
+    do {
+      System.out.println("Ist der Gesuchte zahl:" + guess + "?");
+
+      System.out.println("Ja => Press 1");
+      System.out.println("Nein der zahl ist kleiner => Press 2 ");
+      System.out.println("Nein der zahl ist grösser => Press 3");
+      System.out.println("ihrer Eingabe: ");
+      temp = Integer.parseInt(br.readLine());
+
+      if (temp == 1){
+        System.out.println( "yess, ich hab gewonnen ");
+
+      } else if (temp == 2) {
+        max = guess;
+        guess = nextGuess(guess, max, min);
+      } else if (temp == 3 ) {
+
+        min = guess;
+        guess = nextGuess(guess,max,min);
+
+      } else {
+        System.out.println("Falsche Eingabe");
+      }
+
+    } while (temp != 1);
   }
+
+
+  public static  int nextGuess(int guess,int max, int min) {
+    guess = (max + min) / 2;
+
+    return guess;
+  }
+
+
 }
